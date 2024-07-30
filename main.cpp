eac9384bca3ae0fa028fd43a3682148116eaaae4
avros16 <114757828+avros16@users.noreply.github.com>
2024-07-30 13:19:26 +0000

    Quiz complete

--- main.cpp
+++ main.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 
+using namespace std;
+
+struct Point {
+    int x;
+    int y;
+};
+
+int calculateDifference(int a, int b) {
+    return b - a;
+}
+
 int main() {
-  std::cout << "Hello World!\n";
+    Point p1, p2;
+
+    cout << "Enter the coordinates of the first point in this form (x y) [no comma]: ";
+    cin >> p1.x >> p1.y;
+
+    cout << "Enter the coordinates of the second point in this form (x y) [no comma]: ";
+    cin >> p2.x >> p2.y;
+
+    int xDifference = calculateDifference(p1.x, p2.x);
+    int yDifference = calculateDifference(p1.y, p2.y);
+
+    cout << "The difference in x-coordinates is: " << xDifference << endl;
+    cout << "The difference in y-coordinates is: " << yDifference << endl;
+
+    return 0;
 }
